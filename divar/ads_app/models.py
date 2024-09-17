76963aec382176b0f3134daba8f0c64aaacaa57c
aria <aria.legion2@gmail.com>
2024-09-17 17:26:16 +0330

    added models iin models file

--- divar/ads_app/models.py
+++ divar/ads_app/models.py
@@ -1,3 +1,26 @@
 from django.db import models
+from user_app.models import UserAccount
+
+class Category(models.Model):
+    name = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.name
+    
+class Ad(models.Model):
+    title = models.CharField(max_length=100)
+    description = models.TextField()
+    price = models.IntegerField()
+    location = models.CharField(max_length=255)
+    category = models.ForeignKey(Category , on_delete=models.CASCADE)
+    user = models.ForeignKey(UserAccount, on_delete=models.CASCADE)
+ 
+    def __str__(self):
+        return self.title
+
+
+
+
+
 
 # Create your models here.
